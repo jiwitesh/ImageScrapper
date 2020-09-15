c2b531263af41b3193ae17b3dc77107d7d18f3f0
Amit Sangwan <sangwanankit643@gmail.com>
2020-09-15 12:40:19 +0530

    modified scraper.py

--- ImageScrapper/scraper.py
+++ ImageScrapper/scraper.py
@@ -50,7 +50,8 @@ def fetch_image_urls(query: str, max_links_to_fetch: int, wd: webdriver, sleep_b
         else:
             print("Found:", len(image_urls), "image links, looking for more ...")
             time.sleep(30)
-            return
+
+            # This will click on 'more results' button 
             load_more_button = wd.find_element_by_css_selector(".mye4qd")
             if load_more_button:
                 wd.execute_script("document.querySelector('.mye4qd').click();")
